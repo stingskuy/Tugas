bb3d8ccdec678d120214f83ce62777cc4e83a6c2
stingskuy <115633247+stingskuy@users.noreply.github.com>
2022-10-12 18:55:04 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database Akademik </title>
+	<table border="1" <hr align=“left” size="400px" width="65%"	noshade>     
+            <tr>
+			<th height="100px" colspan="4" aria-setsize="20%">
+                <h1>Layanan Akademik Universitas Jenderal Ahcmad Yani</h1>
+            </th>
+        </tr>
+            <td>    
+            <b>Menu</b>
+                <nav>
+                   
+                      <li><a href="  ">1. Data Mahasiswa</a></li>
+                    
+                  </nav>
+                  
+                  <nav aria-label="breadcrumb">
+                    
+                      <li class="breadcrumb-item"><a href="     ">2. Data Dosen</a></li>
+                    
+                  </nav>
+            
+                  <nav aria-label="breadcrumb">
+                    
+                      <li class="breadcrumb-item"><a href="     ">3. Mata Kuliah</a></li>
+                    
+                  </nav>
+
+                  <nav aria-label="breadcrumb">
+                    
+                      <li class="breadcrumb-item"><a href="     ">4. Jurusan</a></li>
+                    
+                  </nav>
+            </td>
+        <td>
+            <center><img src="LOGO UNJANI.png" alt=""></center>
+        
+           
+        </td>
+        <td>
+            <ul>
+                <b>Kelompok</b>
+                <li><a >Naufal Indra Gunawan </a></li>
+                <li><a >Griyanda Azhar Yustiani</a></li>
+                <li><a >M Naufal Zhafran S</a></li>
+                <li><a >Rizza Nur Firmansyah</a></li>
+            </ul>
+		</td> 
+        </tr>
+        <tr>
+        <center><th colspan="9"; style="background-color:lightgray; text-align: center;"><span><a >SELAMAT DATANG DI LAYANAN AKADEMIK </a></span></th></center>
+    </tr>
+	</table>
+    </head>
+    
+    </body> 
+</html>
